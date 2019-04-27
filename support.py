749c8f7419749d07d6777b1be00fc3d11cf8b0e7
ayasayed <ayasayed_711@yahoo.com>
2019-04-27 17:23:29 +0200

    k

--- support.py
+++ support.py
@@ -1,50 +1,27 @@
-
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 
-img_rgb = cv2.imread('trus3.jpg')
+img_rgb = cv2.imread('trus2.jpg')
+template = cv2.imread('tr.png',0)
+def  support(template,img_rgb) :
+    w, h = template.shape[::-1]
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+    res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+    threshold = 0.8
+    loc = np.where( res >= threshold)
 
-def detectSupport(img_rgb):
-  template1 = cv2.imread('tr.png', 0)
-  template = cv2.imread('tr1.png', 0)
-  template2 = cv2.imread('tr2.png', 0)
-  img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-  w, h = template.shape[::-1]
-  res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+    f = set()
 
-  threshold = 0.8
-  loc = np.where( res >= threshold)
+    for pt in zip(*loc[::-1]):
+        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
 
+        sensitivity = 100
+        f.add((round(pt[0]/sensitivity), round(pt[1]/sensitivity)))
 
+    cv2.imwrite('res1.png',img_rgb)
 
-  for pt in zip(*loc[::-1]):
-       cv2.rectangle(img_rgb,pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
+    found_count = len(f)
+    return found_count
 
-  ##
-  w, h = template1.shape[::-1]
-
-  res = cv2.matchTemplate(img_gray, template1, cv2.TM_CCOEFF_NORMED)
-  threshold = 0.8
-  loc = np.where(res >= threshold)
-
-
-  for pt in zip(*loc[::-1]):
-    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 255, 255), 2)
-  ##
-  w, h = template2.shape[::-1]
-  res = cv2.matchTemplate(img_gray, template2, cv2.TM_CCOEFF_NORMED)
-  threshold = 0.8
-  loc = np.where(res >= threshold)
-
-
-  for pt in zip(*loc[::-1]):
-    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 255, 255), 2)
-
-  cv2.imshow('Detected',img_rgb)
-  plt.imshow(img_rgb)
-  plt.show()
-  cv2.waitKey(0)
-  cv2.destroyAllWindows()
-
-detectSupport(img_rgb)
+print(support(template, img_rgb))
