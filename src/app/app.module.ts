3999f1926ebed38134bbc57bf4fc33d9ccaff785
ayasayed <ayasayed_711@yahoo.com>
2019-04-27 18:00:57 +0200

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeadComponent } from './head/head.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import { MatTabsModule, MatSidenavModule, MatToolbarModule ,MatIconModule,MatListModule , MatButtonModule} from '@angular/material';
+import { HeaderComponent } from './navigation/header/header.component';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
+import { FixedsidnavComponent } from './fixedsidnav/fixedsidnav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadComponent,
+    HeaderComponent,
+    SidenavListComponent,
+    FixedsidnavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatTabsModule,
+     MatSidenavModule,
+     MatToolbarModule,
+     MatIconModule,
+      MatButtonModule,
+      MatListModule
   ],
+  exports:[
+    MatTabsModule,
+    MatSidenavModule,
+     MatToolbarModule,
+     MatIconModule,
+     MatButtonModule,
+     MatListModule
+    ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/navigation/header/header.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit ,Output,EventEmitter} from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  logo = 'assets/images/logo.svg';
+  @Output() public sidenavToggle = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public onToggleSidenav = () => {
+    this.sidenavToggle.emit();
+  }
+
+}
