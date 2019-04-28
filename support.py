04ed8307f8bded6a4be4e18dec4258ade1c273c6
ayasayed <ayasayed_711@yahoo.com>
2019-04-28 03:37:41 +0200

    sa

--- support.py
+++ support.py
@@ -2,8 +2,8 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img_rgb = cv2.imread('trus2.jpg')
-template = cv2.imread('tr.png',0)
+img_rgb = cv2.imread('image.png')
+
 def  support(template,img_rgb) :
     w, h = template.shape[::-1]
     img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
@@ -22,6 +22,19 @@ def  support(template,img_rgb) :
     cv2.imwrite('res1.png',img_rgb)
 
     found_count = len(f)
+
     return found_count
 
-print(support(template, img_rgb))
+def all_support(img_rgb) :
+    template = cv2.imread('tr.png', 0)
+    template1 = cv2.imread('tr1.png', 0)
+    template2 = cv2.imread('tr2.png', 0)
+    i=0
+    i = support(template, img_rgb)
+    i = i+support(template1, img_rgb)
+    i = i+support(template2, img_rgb)
+
+
+    return i;
+
+print(all_support( img_rgb))
