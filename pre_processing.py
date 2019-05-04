9f70d30a3ff298eaa991f3af3e19843bc246b612
Andrew Ishak Ibrahem <33038337+andrewishak@users.noreply.github.com>
2019-05-04 09:40:30 +0200

    def

--- pre_processing.py
+++ pre_processing.py
@@ -1,29 +1,15 @@
-import cv2
-import numpy as np
-############################# Preprocessing part ################################################## 
-imagee = cv2.imread('1.jpg', 1)
-imagee = cv2.cvtColor(imagee, cv2.COLOR_BGR2GRAY)
+def pre (image):
+    dim = (800,400)
 
-width = int(800)
-height = int(400)
-dim = (width, height)
+    # resize image
+    #image = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
+    #noise elimination
+    kernel = np.ones((2, 2), np.uint8)  # kernel for erosion
+    erosion = cv2.erode(image, kernel, iterations=1)  # increase black area
+    t = cv2.fastNlMeansDenoising(erosion, None, 20, 21, 7)  # remove some noise and bluring
 
-# resize image
-image = cv2.resize(imagee, dim, interpolation=cv2.INTER_AREA)
+    # img to binary 
+    image = cv2.medianBlur(t,5)
+    ret,image = cv2.threshold(image,115,255,cv2.THRESH_BINARY)
 
-#noise elimination
-kernel = np.ones((2, 2), np.uint8)  # kernel for erosion
-erosion = cv2.erode(image, kernel, iterations=1)  # increase black area
-t = cv2.fastNlMeansDenoising(erosion, None, 20, 21, 7)  # remove some noise and bluring
-
-# img to binary 
-image = cv2.medianBlur(image,5)
-ret,image = cv2.threshold(image,115,255,cv2.THRESH_BINARY)
-
-
-
-print('Resized Dimensions : ', image.shape)
-cv2.imshow("Resized image", image)
-#cv2.imwrite('11.jpg',image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    return image
